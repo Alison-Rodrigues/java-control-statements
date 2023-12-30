b7bcb230878ea3592d269f2e18d7f3893da369fe
Alison Rodrigues <alison.si1302@gmail.com>
2023-12-30 01:46:28 -0300

    initial commit

---
+++ src/main/java/com/estudo/controlstatements/ControlStatementsApplication.java
@@ -0,0 +1,13 @@
+package com.estudo.controlstatements;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlStatementsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlStatementsApplication.class, args);
+	}
+
+}
